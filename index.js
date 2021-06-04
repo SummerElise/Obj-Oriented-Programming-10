487028febbdf59f64e51bff2981bd0302181d823
lissy <slissy513@gmail.com>
2021-06-04 15:40:20 -0400

    added code to index.js

--- index.js
+++ index.js
@@ -6,3 +6,26 @@ const Engineer = require("./lib/Engineer")
 const Manager = require("./lib/Manager")
 const Intern = require("./lib/Intern")
 
+inquirer
+.prompt([
+    {
+        type: 'input',
+        name: 'name',
+        message: 'What is the Managers name?',
+    },
+    {
+        type: 'input',
+        name: 'ID',
+        message: 'What is the employee ID?',
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What is the Managers email?',
+    },
+    {
+        type: 'input',
+        name: 'Office number',
+        message: 'What is the offices telephone number?',
+    },
+]);
