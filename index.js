08099bad1a8670a8bfb84b54fa9c3c5a809e7e45
lissy <slissy513@gmail.com>
2021-06-07 12:58:19 -0400

    added code to render html in index.js file; made a final.html file; fixed syntax errors in files in lib folder

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
 
-const Employee = require("./lib/Employee")
+const employees = [];
 const Engineer = require("./lib/Engineer")
 const Manager = require("./lib/Manager")
 const Intern = require("./lib/Intern")
 
+function initApp() {
+    topHTML();
+    addMember();
+}
+
+function addMember() {
 inquirer
 .prompt([
     {
@@ -47,10 +53,10 @@ inquirer
         type: 'list',
         message: 'Do you want to add more team members?',
         choices: ['yes', 'no'],
-        name: 'addMembers'
+        name: 'moreMembers'
     }])
 
-.then(function({roleInput, addMembers}) {
+.then(function({roleInput, moreMembers}) {
     let newMember;
     if (role === 'Manager') {
         newMember = new Manager(name, ID, email, roleInput);
@@ -60,5 +66,48 @@ inquirer
         newMember = new Intern(name, ID, email, roleInput);
     }
     employees.push(newMember);
-    addHTML
-    
+    addHTML(newMember)
+    .then(function() {
+        if (moreMembers === 'yes') {
+            addMember();
+        } else {
+            bottomHTML();
+        }
+        });
+    });
+});
+}
+    function topHTML() {
+        const HTML = `< !DOCTYPE html >
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <meta http-equiv="X-UA-Compatible" content="ie=edge">
+            <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
+            integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+            <title>Team Profile</title>
+        </head>
+        <body>
+        <nav class="navbar navbar-dark bg-dark mb-5">
+        <span class="navbar-brand mb-0 h1 w-100 text-center">Team Members</span>
+    </nav>
+    <div class="container">
+        <div class="row">`;
+        fs.writeFile('./final.html', html)
+        console.log('top');
+    }
+
+    function addHTML(member) {
+        return new Promise(function(res, rej) {
+            const name = member.getName();
+            const role = member.getRole();
+            const ID = member.getID();
+            const email = member.getEmail();
+            let data = "";
+            if (role === 'Manager') {
+                const office = member.getOfficeNumber();
+                data = ``
+            }
+        })
+    }
